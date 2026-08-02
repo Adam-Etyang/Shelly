ea0584d794567a06552eb4adf760ef42a907a76e
Adam-Etyang <adametyang69@icloud.com>
2026-08-02 13:26:31 +0300

    Builtin commands header

---
+++ include/builtins.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+#include <string>
+class Builtins{
+  public:
+    static void cd(std::vector<std::string>& args);
+    static void echo(std::vector<std::string>& args);
+    static void type(std::vector<std::string>& args);
+    static void pwd(std::vector<std::string>& args);
+    static int  redirect(std::vector<std::string>& args);
+}
