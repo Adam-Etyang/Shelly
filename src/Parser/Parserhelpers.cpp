261e486b9092d4b3ea95dd9c655b604c65a126fe
Adam-Etyang <adametyang69@icloud.com>
2026-08-20 11:20:28 +0300

    Parser helpers

--- src/Parser/Parserhelpers.cpp
+++ src/Parser/Parserhelpers.cpp
@@ -1,19 +1,36 @@
 #include "parser.hpp"
+#include <cassert>
 
-// retutns the token at current pos
-Token Parser::peek() {}
+// returns the token at current pos
+Token Parser::peek() {
+  assert(pos < tokens.size());
+  return tokens[pos];
+}
 
 // returns token that was consumed
-Token Parser::advance() { return }
+Token Parser::advance() {
+  assert(pos < tokens.size());
+  Token current = tokens[pos];
+  pos++;
+  return current;
+}
 
 /*Returns true if not at end,
 the current token (peek()) is unquoted,
 and its .text equals text.
 */
-bool Parser::Check(const std::string &val) {}
+bool Parser::Check(const std::string &val) {
+  return pos < tokens.size() && !tokens[pos].quoted && tokens[pos].text == val);
+}
 
 /*Calls check(text);
 if true, calls advance() and returns true;
 if false, does nothing and returns false.
 */
-bool Parser::Match(const std::string &val) {}
+bool Parser::Match(const std::string &val) {
+  if (Check(val)) {
+    advance();
+    return true;
+  }
+  return false;
+}
