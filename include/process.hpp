0b7989637bd5ec987859dea7afb3baf7f7f50d3d
Adam-Etyang <adametyang69@icloud.com>
2026-08-28 23:54:42 +0300

    New enums and helper functions

--- include/process.hpp
+++ include/process.hpp
@@ -1,20 +1,35 @@
 #pragma once
 #include <string>
+#include <sys/_types/_pid_t.h>
 #include <sys/types.h>
 #include <vector>
 
+enum class JobStatus { Running, Done, Stopped };
+
 struct Job {
   pid_t pid;
+  pid_t pgid;
   std::string command;
-  bool done = false;
+  JobStatus status;
+  int lastStatus;
 };
 
 class Process {
 public:
-  bool exec(std::vector<std::string>& args);
+  bool exec(std::vector<std::string> &args);
   void addBackgroundJob(pid_t pid, const std::string &command);
   void reapJobs();
   void printJobs();
+
+  Job *findBySpec(const std::string &spec);
+  Job *currentJob();
+  Job *previousJob();
+  Job *findBYPID(pid_t pid);
+  Job *findByPGID(pid_t pgid);
+  void updateState(pid_t pid, JobStatus status, int lastStatus);
+
+  bool fg(const std::string &spec);
+  bool bg(const std::string &spec);
 
 private:
   std::vector<Job> jobs_;
